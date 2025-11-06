c9d9ea1c1a2ee9462afd35c2ef1a92042e2a7fab
Gonzalo Castro <cate@MacBook-Air-de-Gonzalo.local>
2025-11-06 20:37:55 -0300

    feat(compra): mostrar 'Vendido' en rojo en sección de autos vendidos

--- src/app/compra/CatalogClient.tsx
+++ src/app/compra/CatalogClient.tsx
@@ -200,7 +200,7 @@ export default function CatalogClient({ vehicles }: CatalogClientProps) {
           {soldVehicles.length > 0 ? (
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
               {soldVehicles.map((vehicle) => (
-                <VehicleCard key={`sold-${vehicle.id}`} vehicle={vehicle} hideDetailsCTA />
+                <VehicleCard key={`sold-${vehicle.id}`} vehicle={vehicle} hideDetailsCTA sold />
               ))}
             </div>
           ) : (
