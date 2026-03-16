cba6f372f9b7ea5fe16106ca7eda32bf02dcbbec
Gonzalo Castro <gonzaloacastroz@gmail.com>
2026-03-16 11:49:18 -0300

    install google analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   const whatsappPhone = process.env.NEXT_PUBLIC_WHATSAPP_PHONE;
   return (
     <html lang="es">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TRWPW11NPK"></script>
+        <script>
+          {`
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-TRWPW11NPK');
+          `}
+        </script>
+      </head>
       <body className="bg-white text-gray-900 antialiased font-body">
         {/* Header global */}
         <Header />
